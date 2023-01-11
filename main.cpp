2d96824a0c0def2fc0a7c12d564f6489c4e1be33
alexsony <alexsony1997@gmail.com>
2023-01-11 17:16:41 +0200

    Plot library added.

--- main.cpp
+++ main.cpp
@@ -1,25 +1,51 @@
 #include <iostream>
 #include <unistd.h>
+#include <set>
+#include <matplot/matplot.h>
 
 #include "CPUParser.hpp"
 #include "MemoryParser.hpp"
 #include "NetParser.hpp"
 
-int main() {
-    CpuParser cpu;
-    MemoryParser memory;
-    NetParser  netO;
-    Net net;
-    while(1) {
-        net = netO.usage();
-        std::cout << cpu.name() << std::endl;
-        std::cout << "CPU: " << cpu.usage() << "%" << std::endl;
-        std::cout << "Memory:"<< memory.usage().first << "Mb of " << memory.total() << "Mb" << std::endl; 
+using namespace matplot;
 
-        for (const auto &entry: net) {
-            std::cout << "Interface "<< entry.first <<": " <<  entry.second.first <<"Kbs " << entry.second.second <<"Kbs"<<std::endl;
+int main() {
+    CpuParser cpuObject;
+    MemoryParser memoryObject;
+    NetParser  netObject;
+    Net net;
+
+    std::vector<int> Y;
+    std::vector<int> cpu;
+    int i = 0;
+
+    auto h = figure();
+    auto ax = h->current_axes();
+    axis({0, 60, 0, 100});
+
+    while(1) {
+        if (i < 60) {
+            Y.push_back(i);
+            i++;
+        } else {
+            cpu.erase(cpu.begin());
         }
-        std::cout << "------------------------------------" << std::endl;
+        
+        cpu.push_back(cpuObject.usage());
+        plot(ax, Y, cpu);
         sleep(1);
     }
+
+    // while(1) {
+    //     // net = netO.usage();
+    //     // std::cout << cpu.name() << std::endl;
+    //     // std::cout << "CPU: " << cpu.usage() << "%" << std::endl;
+    //     // std::cout << "Memory:"<< memory.usage().first << "Mb of " << memory.total() << "Mb" << std::endl; 
+
+    //     // for (const auto &entry: net) {
+    //     //     std::cout << "Interface "<< entry.first <<": " <<  entry.second.first <<"Kbs " << entry.second.second <<"Kbs"<<std::endl;
+    //     // }
+    //     // std::cout << "------------------------------------" << std::endl;
+    //     // sleep(1);
+    // }
 }
